ea30c2515482a02de3a9072b24d7c3b354267242
Glitch (mongodb-sync) <none>
2019-06-12 15:24:26 +0000

    👞👯 Checkpoint ./utils/auth.js:3223539/1673

--- utils/auth.js
+++ utils/auth.js
@@ -28,16 +28,72 @@ const signup = async(req, res) => {
   
   try{
     const user = await User.create
+    const token = newToken(user)
+    return res.status(201).send({token})
+  } catch (e){
+    return res.status(500).end()
   }
 }
+
+module.exports = signup
+  
+const signin = async (req, res) => {
+  if(!req.body.email || !req.body.password) {
+    return res.status(400).send({message: 'need email and password'})
+  }
+  const invalid = {message: 'Invalid email and password combo, try again!'}
+  
+  try{
+    const user = await User.findOne({ email: req.body.email})
+      .select('email password')
+      .exec() 
+    
+    if(!user){
+      return res.status(401).send(invalid)
+    }
+    
+    const match = await user.checkPassword(req.body.password)
+    
+    if(!match){
+      return res.status(401).send({token})
+    }
+    
+    const token = newToken(user)
+    return res.status(201).send({token})
+  } catch(e){
+    console.error(e)
+    res.status(500).end()
+  }
 }
 
-// var m;
-// try {
-//     m = require(modulePath);
-// } catch (e) {
-//     if (e.code !== 'MODULE_NOT_FOUND') {
-//         throw e;
-//     }
-//     m = backupModule;
-// }
+module.exports = signin 
+
+const protect = async (req, res, next) => {
+  const bearer = req.headers.authorization
+
+  if (!bearer || !bearer.startsWith('Bearer ')) {
+    return res.status(401).end()
+  }
+
+  const token = bearer.split('Bearer ')[1].trim()
+  let payload
+  try {
+    payload = await verifyToken(token)
+  } catch (e) {
+    return res.status(401).end()
+  }
+
+  const user = await User.findById(payload.id)
+    .select('-password')
+    .lean()
+    .exec()
+
+  if (!user) {
+    return res.status(401).end()
+  }
+
+  req.user = user
+  next()
+}
+
+module.exports = protect 
