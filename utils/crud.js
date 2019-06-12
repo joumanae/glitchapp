54970c6e6f0612302dc4815aba3476eea579d731
Glitch (mongodb-sync) <none>
2019-06-12 21:08:34 +0000

    🎽👑 Checkpoint ./utils/crud.js:3223539/2448

--- utils/crud.js
+++ utils/crud.js
@@ -1,18 +1,96 @@
 const getOne = model => async (req, res) => {
-  try{
+  try {
     const doc = await model
-      .findOne({createdBy: req.user._id, _id: req.params.id })
+      .findOne({ createdBy: req.user._id, _id: req.params.id })
       .lean()
-      .exec() 
-    if(!doc){
-      return res.status(400).end() 
+      .exec()
+
+    if (!doc) {
+      return res.status(400).end()
     }
-    
-    res.status(200).json({data: doc})
-  } catch(e){
+
+    res.status(200).json({ data: doc })
+  } catch (e) {
     console.error(e)
     res.status(400).end()
   }
 }
 
-module.exports = getOne 
+const getMany = model => async (req, res) => {
+  try {
+    const docs = await model
+      .find({ createdBy: req.user._id })
+      .lean()
+      .exec()
+
+    res.status(200).json({ data: docs })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+const createOne = model => async (req, res) => {
+  const createdBy = req.user._id
+  try {
+    const doc = await model.create({ ...req.body, createdBy })
+    res.status(201).json({ data: doc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+const updateOne = model => async (req, res) => {
+  try {
+    const updatedDoc = await model
+      .findOneAndUpdate(
+        {
+          createdBy: req.user._id,
+          _id: req.params.id
+        },
+        req.body,
+        { new: true }
+      )
+      .lean()
+      .exec()
+
+    if (!updatedDoc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: updatedDoc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+const removeOne = model => async (req, res) => {
+  try {
+    const removed = await model.findOneAndRemove({
+      createdBy: req.user._id,
+      _id: req.params.id
+    })
+
+    if (!removed) {
+      return res.status(400).end()
+    }
+
+    return res.status(200).json({ data: removed })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+const crudControllers = model => ({
+  removeOne: removeOne(model),
+  updateOne: updateOne(model),
+  getMany: getMany(model),
+  getOne: getOne(model),
+  createOne: createOne(model)
+})
+
+
+module.exports = {removeOne, updateOne, getOne, getMany, getOne, createOne, crudControllers}
