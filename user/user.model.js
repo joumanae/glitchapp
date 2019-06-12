33f0ab7ca64747ce6f7c1175e15005e2790dff49
Glitch (mongodb-sync) <none>
2019-06-12 15:46:36 +0000

    🎤💶 Checkpoint ./user/user.model.js:3223539/1352

--- user/user.model.js
+++ user/user.model.js
@@ -39,20 +39,34 @@ const userSchema = new mongoose.Schema(
 
 )
 
-userSchema.pre('save', function(next){
-  if (!this.isModified('password')){
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
     return next()
   }
-  
-  bcrypt.hash(this.password, 8, (err,hash) => {
-    if (err){
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) {
       return next(err)
     }
+
     this.password = hash
-    next() 
+    next()
   })
 })
 
 userSchema.methods.checkPassword = function(password) {
-  
+  const passwordHash = this.password
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve(same)
+    })
+  })
 }
+
+const User = mongoose.model('user, userSchema')
+
+module.exports = User 
