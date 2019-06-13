0c85780c439f76a0a0ed00c93cc2f21d4b431596
Glitch (getshiftdone) <none>
2019-06-13 21:07:54 +0000

    🐲💖 Checkpoint ./shift/shift.model.js:3223539/112 ./utils/auth.js:3223539/2

--- shift/shift.model.js
+++ shift/shift.model.js
@@ -20,11 +20,6 @@ const shiftSchema = new mongoose.Schema(
       trim: true 
       
     }, 
-    createdBy:{
-      type: mongoose.SchemaTypes.ObjectId,
-      required: true, 
-      ref: 'user'
-    }, 
       list: {
       type: mongoose.SchemaTypes.ObjectId,
       ref: 'list',
